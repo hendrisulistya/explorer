46719d17e246c2e19e95d93c6262e27c87624232
Hendri Sulistya <id.hendrisulistya@gmail.com>
2022-09-28 15:24:54 +0700

    gas fee final

--- src/views/WalletAccountImportAddress.vue
+++ src/views/WalletAccountImportAddress.vue
@@ -504,9 +504,9 @@ export default {
           coinGeckoId: chain.assets[0].coingecko_id || 'unknown',
         },
         gasPriceStep: {
-          low: 200000000000,
-          average: 500000000000,
-          high: 1500000000000,
+          low: 800000000000,
+          average: 2000000000000,
+          high: 6000000000000,
         },
         features: chain.keplr_features || [],
       }, null, '\t')
